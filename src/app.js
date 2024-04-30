198fc59dcf0bba5bae666f3ac55d293026cda593
sonaly-mehr <sonaly.mehr@gmail.com>
2024-04-30 22:40:42 +0600

    updated

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('https://doctors-portal-backend-f9sn.onrender.com', routes)
+app.use('/', routes)
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 app.get('/', async (req, res, next) => {
